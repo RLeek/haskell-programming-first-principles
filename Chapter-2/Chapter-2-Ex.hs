cfc1376e566c812a84b60eff90bf3f2799d5b51b
Ricky <rky.w.liu@gmail.com>
2024-12-05 22:08:31 +1100

    Remove file

--- Chapter-2/Chapter-2-Ex.hs
+++ Chapter-2/Chapter-2-Ex.hs
@@ -1,5 +1,27 @@
 module Chapter2Ex where
 
+-- Ex: A Head Code
+-- 1.
+eq1 = x *3 + y
+ where x = 3
+       y = 1000
+
+-- 2.
+eq2 = x *5 
+ where x = 10 *5 + y
+       y = 10
+
+
+-- 3.
+eq3 = x *y
+ where x = 5
+       y = 6
+
+-- 4.
+eq4 = x + 3
+ where x = 3
+       y = 1000
+
 -- Ex: Parenthesization
 -- 1. 2 + (2*3) - 1
 -- 2. (^) 10 $ (1 + 1)
